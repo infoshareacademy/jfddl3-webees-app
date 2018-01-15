f4228e560a182fe33b655c55691bd02e12f6ba23
Aleksandra Jagodzińska <aleksandra.berry01@gmail.com>
2018-01-15 17:01:17 +0100

    Headin to slider added

--- src/List.js
+++ src/List.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import WebeesPaper from './WebeesPaper'
+import Slider from 'material-ui/Slider';
+
+const SliderExampleStep = () => (
+    <Slider step={0.10} value={0.5} />
+);
 
 const List = () => (
     <div>
         <WebeesPaper>
-            <b>asdfghjkxrtdfyguhj</b>
+            <h1>Dystans</h1>
+        <div style={{textAlign:'left', padding:'50'}}>0 km</div>
+            <div>
+            <Slider
+                min={0}
+                max={50}/>
+            </div>
+            <div style={{textAlign:'right'}}>50km</div>
         </WebeesPaper>
     </div>
 )
