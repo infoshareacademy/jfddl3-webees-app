f8b332cc1322e2ec56193752fa08651fe268b09b
Paweł Lorencowicz <p.lorencowicz@gmail.com>
2018-01-16 17:44:36 +0100

    firebase added

--- src/AddRun.js
+++ src/AddRun.js
@@ -1,33 +1,20 @@
 import React, {Component} from 'react'
-import { render } from 'react-dom'
+import {render} from 'react-dom'
 import GoogleMapReact from 'google-map-react'
 
 const Pin = () => <div><i className="material-icons" style={{color: 'red'}}>room</i></div>
 
 class Map extends React.Component {
-    state = {
-        markers: []
-    }
-
-    placeMarker = ({ lat, lng }) => {
-        const markerData = {lat, lng, key: Date.now()}
-        this.setState({
-            markers: this.state.markers.concat()
-        }, ()=>{
-            localStorage.setItem('key', JSON.stringify(markerData)) // not working !
-        })
-    }
-
     render() {
         return (
             <GoogleMapReact
                 apiKey={'AIzaSyBjbSX619TpTJBp9afQKJUuueKAF9ZGawc'}
                 defaultCenter={this.props.center}
                 defaultZoom={this.props.zoom}
-                onClick={this.placeMarker}
+                onClick={this.props.placeMarker}
             >
                 {
-                    this.state.markers.map((marker) =>
+                    this.props.markers.map((marker) =>
                         <Pin
                             key={marker.key}
                             lat={marker.lat}
@@ -40,13 +27,42 @@ class Map extends React.Component {
     }
 }
 
-const AddRun = () => (
-    <div style={{ width: '50vw', height: '50vh' }}>
-        <Map
-            center={{ lat: 51.216276, lng: 22.631233 }}
-            zoom={15}
-        />
-    </div>
-)
+
+class AddRun extends React.Component {
+    state = {
+        markers: [],
+        name: '',
+        type: ''
+    }
+
+    save = () => {
+        fetch('https://my-project-15d7d.firebaseio.com/.json', {
+            method: 'POST',
+            body: JSON.stringify(this.state.markers),
+            headers: ({'Content-Type': 'application/json'})
+        })
+    }
+
+    placeMarker = ({lat, lng}) => {
+        const markerData = {lat, lng, key: Date.now()}
+        this.setState({
+            markers: this.state.markers.concat(markerData)
+        })
+    }
+
+    render() {
+
+        return (
+            <div style={{width: '60vw', height: '60vh'}}>
+                <Map
+                    center={{lat: 51.216276, lng: 22.631233}}
+                    zoom={15}
+                    markers={this.state.markers}
+                    placeMarker={this.placeMarker}
+                />
+            </div>
+        )
+    }
+}
 
 export default AddRun;
