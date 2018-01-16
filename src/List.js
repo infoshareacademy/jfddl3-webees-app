994341f55835e782ea98cd969f5f1f629c63416a
Aleksandra Jagodzińska <aleksandra.berry01@gmail.com>
2018-01-16 17:18:45 +0100

    Dropdown added

--- src/List.js
+++ src/List.js
@@ -1,24 +1,39 @@
 import React from 'react'
 import WebeesPaper from './WebeesPaper'
 import Slider from 'material-ui/Slider';
+import DropDownMenu from 'material-ui/DropDownMenu';
+import MenuItem from 'material-ui/MenuItem';
+
 
 const SliderExampleStep = () => (
-    <Slider step={0.10} value={0.5} />
+    <Slider step={0.10} value={0.5}/>
 );
 
-const List = () => (
+
+class List {
     <div>
         <WebeesPaper>
             <h1>Dystans</h1>
-        <div style={{textAlign:'left', padding:'50'}}>0 km</div>
+<div style={{textAlign: 'left', padding: '50'}}>0 km</div>
             <div>
-            <Slider
-                min={0}
-                max={50}/>
+<Slider
+min={0}
+max={50}/>
             </div>
-            <div style={{textAlign:'right'}}>50km</div>
+
+
+<div>
+<DropDownMenu value={this.state.value} onChange={this.handleChange}>
+<MenuItem value={1} primaryText="Bieg po lesie"/>
+<MenuItem value={2} primaryText="Bieg po mieście"/>
+</DropDownMenu>
+<br/>
+
+</div>
+
+<div style={{textAlign: 'right'}}>50km</div>
         </WebeesPaper>
     </div>
 )
-
+}
 export default List
