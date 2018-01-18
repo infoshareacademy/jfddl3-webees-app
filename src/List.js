d16706dbb4261398a2f380c45ef6ff215c6b6af7
Aleksandra Jagodzińska <aleksandra.berry01@gmail.com>
2018-01-18 16:21:36 +0100

    Text Field added

--- src/List.js
+++ src/List.js
@@ -3,17 +3,19 @@ import WebeesPaper from './WebeesPaper'
 import Slider from 'material-ui/Slider';
 import DropDownMenu from 'material-ui/DropDownMenu';
 import MenuItem from 'material-ui/MenuItem';
+import TextField from 'material-ui/TextField';
+
 
 class List extends React.Component {
-    state = {value: ''}
+    state = {value: ''};
 
-    handleChange = (e, k, v) => this.setState({value: ''})
+    handleChange = (e, index, value) => this.setState({value: value});
 
     render() {
         return (
             <div>
                 <WebeesPaper>
-                    <h1>Dystans</h1>
+                    <h2>Dystans</h2>
                     <div style={{textAlign: 'left'}}>0 km</div>
                     <div>
                         <Slider
@@ -21,13 +23,20 @@ class List extends React.Component {
                             max={50}/>
                     </div>
                     <div style={{textAlign: 'right'}}>50km</div>
+                    <h2>Wybierz rodzaj biegu</h2>
                     <div>
                         <DropDownMenu value={this.state.value} onChange={this.handleChange}>
                             <MenuItem value={1} primaryText="Bieg po lesie"/>
                             <MenuItem value={2} primaryText="Bieg po mieście"/>
                         </DropDownMenu>
                         <br/>
-
+                    </div>
+                    <h2>Wyszukaj bieg</h2>
+                    <div>
+                        <TextField
+                            hintText="Szukaj"
+                            fullWidth={true}
+                        />
                     </div>
                 </WebeesPaper>
             </div>
