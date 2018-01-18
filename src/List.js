47bf56559fc8356491c52b5e8da4f5ad435d16a6
Aleksandra Jagodzińska <aleksandra.berry01@gmail.com>
2018-01-18 18:25:27 +0100

    List Search file added

--- src/List.js
+++ src/List.js
@@ -1,9 +1,5 @@
 import React from 'react'
-import WebeesPaper from './WebeesPaper'
-import Slider from 'material-ui/Slider';
-import DropDownMenu from 'material-ui/DropDownMenu';
-import MenuItem from 'material-ui/MenuItem';
-import TextField from 'material-ui/TextField';
+import ListSearch from './ListSearch'
 
 
 class List extends React.Component {
@@ -17,44 +13,15 @@ class List extends React.Component {
 
     render() {
         return (
-            <div>
-                <WebeesPaper>
-                    <div>
-                        <TextField
-                            onChange={this.handleNameChange}
-                            hintText="Wpisz nazwę biegu"
-                            fullWidth={true}
-                        />
-                        <div>
-                            <div style={{display: 'inline-block', textAlign: 'left', width: '10%'}}>0 km</div>
-                            <div style={{display: 'inline-block', width: '80%'}}>
-                                <Slider
-                                    sliderStyle={{marginBottom: 0}}
-                                    min={0}
-                                    max={50}
-                                    onChange={this.handleDistanceChange}
-                                />
-                            </div>
-                            <div style={{display: 'inline-block', textAlign: 'right', width: '10%'}}>50km</div>
-                        </div>
-                        <div>
-                            <DropDownMenu
-                                autoWidth={false}
-                                style={{width: '100%'}}
-                                value={this.state.category}
-                                onChange={this.handleCategoryChange}
-                            >
-                                <MenuItem value={''} primaryText="Wybierz bieg"/>
-                                <MenuItem value={'city'} primaryText="Bieg po lesie"/>
-                                <MenuItem value={'forest'} primaryText="Bieg po mieście"/>
-                            </DropDownMenu>
-                            <br/>
-                        </div>
-                    </div>
-                </WebeesPaper>
-            </div>
+            <ListSearch
+                handleCategoryChange={this.handleCategoryChange}
+                handleNameChange={this.handleNameChange}
+                handleDistanceChange={this.handleDistanceChange}
+                category={this.state.category}
+            />
         )
     }
 }
+
 
 export default List
