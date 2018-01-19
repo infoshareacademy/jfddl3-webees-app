37d435aae6b8d6121a5f9f7f64860e77aabed901
Grzegorz <rc36@wp.pl>
2018-01-19 09:18:30 +0100

    Add BarChard, PieChart, ReLineChart. IT WORKS !!!

--- src/BarChart.js
+++ src/BarChart.js
@@ -7,7 +7,6 @@ const data = [
       {name: 'Dystans_3', uv: 2780, pv: 3908, amt: 2000},
       {name: 'Dystans_4', uv: 1890, pv: 4800, amt: 2181},
       {name: 'Dystans_5', uv: 1600  , pv: 4000, amt: 2300},
-
 ];
 
 const NewBarChart = () => {
